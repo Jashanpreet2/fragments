aaf72bc5249ecd1698402de93780ead4afe1849d
Jashanpreet Singh <jashan20preet21@gmail.com>
2025-03-22 03:37:00 -0400

    Remove unused code

--- utils.go
+++ utils.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"strings"
 
-	"github.com/gin-gonic/gin"
 	cognitoJwtVerify "github.com/jhosan7/cognito-jwt-verify"
 
 	"github.com/gomarkdown/markdown"
@@ -21,53 +20,6 @@ func GetBody(bodyBytes []byte) map[string]interface{} {
 		log.Fatal(err)
 	}
 	return jsonMap
-}
-
-func GetUsername(c *gin.Context, local bool) (string, bool) {
-	if local {
-		if username, _, ok := c.Request.BasicAuth(); ok {
-			return username, true
-		} else {
-			return "", false
-		}
-	} else {
-		cognitoConfig := cognitoJwtVerify.Config{
-			UserPoolId: os.Getenv("AWS_COGNITO_POOL_ID"),
-			ClientId:   os.Getenv("AWS_COGNITO_CLIENT_ID"),
-			TokenUse:   "id",
-		}
-
-		// sugar.Info("Authentication in process")
-		token := c.GetHeader("Authorization")
-		if !strings.HasPrefix(token, "Bearer ") {
-			return "", false
-		}
-
-		token = token[len("Bearer "):]
-		if token == "" {
-			return "", false
-		}
-
-		verify, err := cognitoJwtVerify.Create(cognitoConfig)
-		if err != nil {
-			return "", false
-		}
-
-		payload, err := verify.Verify(token)
-		if err != nil {
-			return "", false
-		}
-
-		jsonData, err := json.Marshal(payload)
-		if err != nil {
-			return "", false
-		}
-
-		sugar.Info(jsonData)
-		var v map[string]string
-		json.Unmarshal(jsonData, &v)
-		return v["cognito:username"], true
-	}
 }
 
 func ConvertMdToHtml(data []byte) []byte {
